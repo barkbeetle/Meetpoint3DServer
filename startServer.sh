d43c46b93f15a594892d4d880a6a07d0f93ca1bd
Sandro Ropelato <sandro.ropelato@gmail.com>
2012-10-19 18:49:21 +0200

    Modified start and stop scripts.

--- startServer.sh
+++ startServer.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-nohup java -cp ./out/production/MeetpointServer net.ropelato.meetpoint.server.Server > log.txt &
-
-
+basedir=`dirname "$0"`
+nohup java -cp "$basedir/out/production/MeetpointServer" net.ropelato.meetpoint.server.Server > "$basedir/log.txt" &
--- stopServer.sh
+++ stopServer.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-kill -9 `lsof -w -n -i tcp:63387 | grep java | awk '{print $2}'`
+pid=`lsof -w -n -i tcp:63387 | grep java | awk '{print $2}'`
+if [ -n "$pid" ]
+then
+	echo "Meetpoint3DServer is running with process id $pid."
+	echo "Trying to kill process $pid..."
+	kill -9 $pid
+	
+	pid=`lsof -w -n -i tcp:63387 | grep java | awk "{print $2}"`
+	if [ -z "$pid" ]
+	then
+		echo "Meetpoint3DServer has been stopped."
+	else
+		echo "Could not stop Meetpoint3DServer."
+	fi
+else
+	echo 'Meetpoint3DServer has not been started.'
+fi
